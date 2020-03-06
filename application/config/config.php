c94d9f0e3cec5ada55eeed37afd98f8bea52934a
rizaldi <rizaldi354313@gmail.com>
2020-03-07 06:36:51 +0700

    aa

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/indoordisplay/';
+$config['base_url'] = 'http://103.247.11.149/indoordisplay/';
 
 /*
 |--------------------------------------------------------------------------
