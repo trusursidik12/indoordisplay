4adc5fe8734b393c5f7e14a674c43dca5f65ce31
warih suryono <warih_suryono@yahoo.com>
2020-03-14 17:01:47 +0700

    aaa

--- application/controllers/Export.php
+++ application/controllers/Export.php
@@ -3,12 +3,11 @@ class Export extends CI_Controller {
 
 	public function index()
 	{
-			echo "Sss";
-		// $indoor_groups = $this->getdata_m->get_indoor_groups($_GET["apikey"]);
-		// foreach($indoor_groups as $key => $indoor_group){
-			// $id_stasiuns[] = $indoor_group["id_stasiun"];
-		// }
-		// $data['alldata'] = $this->getdata_m->getalldata($id_stasiuns);
-		// $this->load->view('stasiun/Report_v', $data);
+		$indoor_groups = $this->getdata_m->get_indoor_groups($_GET["apikey"]);
+		foreach($indoor_groups as $key => $indoor_group){
+			$id_stasiuns[] = $indoor_group["id_stasiun"];
+		}
+		$data['alldata'] = $this->getdata_m->getalldata($id_stasiuns);
+		$this->load->view('stasiun/Report_v', $data);
 	}
 }
